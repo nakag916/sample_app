cf35fe4febfb630e35c96872796c1a7a2c12cb64
Hiroyuki Nakajima <hiroyuki.nakajima@theport.jp>
2019-05-16 12:15:46 +0900

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  get 'signup', to: 'users#new'
 
-  get 'static_pages/help'
+  get '/help', to: 'static_pages#help'
 
-  get 'static_pages/about'
+  get '/about', to: 'static_pages#about'
+ 
+  get '/contact', to: 'static_pages#contact'
  
   root 'static_pages#home'
 end
