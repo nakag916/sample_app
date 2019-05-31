d67990962e5f4fa7d780365ca716c127184f800d
Hiroyuki Nakajima <hiroyuki.nakajima@theport.jp>
2019-05-31 15:09:45 +0900

    Implement basic login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,5 +8,10 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
+  # テストユーザーがログイン中の場合にtrueを返す
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
+
   # Add more helper methods to be used by all tests here...
 end
