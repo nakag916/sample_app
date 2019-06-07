f5fbd06045eb13fc52cc2a60a91eaaf4eb8ed5f6
Hiroyuki Nakajima <hiroyuki.nakajima@theport.jp>
2019-06-07 15:54:29 +0900

    Add user following

--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,5 @@
+#require File.expand_path('../boot',__FILE__)
+
 require_relative 'boot'
 
 require 'rails/all'
@@ -14,5 +16,8 @@ module SampleApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    #認証トークンをremoteフォームに埋め込む
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
